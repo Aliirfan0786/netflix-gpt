37a4c82fe6446ae39668fd13cfadd094527e3c11
Irfan Ali <ia.ali053263@gmail.com>
2024-09-21 17:12:50 +0530

    created movie lists section

--- src/components/SecondContainer.js
+++ src/components/SecondContainer.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondContainer = () => {
-  return (
-    <div>SecondContainer</div>
+  const movies = useSelector(store=>store.movies);
+  return movies.nowPlayingMovies && (
+    <div className=' bg-black'>
+      <div className='-mt-52 relative z-20 pl-16'>
+      <MovieList  title ={"Now Playing"}movies = {movies.nowPlayingMovies} />
+      <MovieList  title ={"Popular"}movies = {movies.nowPlayingMovies} />
+      <MovieList  title ={"Top Rated"}movies = {movies.nowPlayingMovies} />
+      <MovieList  title ={"Upcoming"}movies = {movies.nowPlayingMovies} />
+      </div>
+
+
+
+
+    </div>
   )
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,4 @@
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjU5NTdjY2ViNzQ2YzIwYjQ0ZmIwZjgzZmM3Mjk2MyIsIm5iZiI6MTcyNjgyOTU0NC4yNTY3MzYsInN1YiI6IjY2ZWQ0ZmM5MmRjYjc1OGJiZmFkYmYzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iq2Kg0dLA5T8nGOfpUCpPN-FNAsQj3dB8v0HUyus4lA'
     }
   };
+  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780/";
